ba30ba242e7a78a0288697c7a49357d0f76f3f08
ysyonline <937026716@qq.com>
2016-12-24 22:02:44 +0800

    prototypeTest.js modify

--- prototypTest.js
+++ prototypTest.js
@@ -26,23 +26,93 @@
     //=====================isPrototyeOf==================
     (function(){
         log(arguments[0]);
+        function Person( name, age ){
+            this.name = name;
+            this.age = age;
+        }
+        Person.prototype.say = function(){
+            console.log(this.name);
+        }
 
+        var p = new Person( 'a', 32 );
+        var flag = Person.prototype.isPrototypeOf(p);
+        console.log(flag);
     })('isPrototyeOf');
     //=====================getPrototyeOf==================
     (function(){
         log(arguments[0]);
+        function Person( name, age ){
+            this.name = name;
+            this.age = age;
+        }
+        Person.prototype.say = function(){
+            console.log(this.name);
+        }
 
+        var p = new Person( 'a', 32 );
+        var p_prototype = Object.getPrototypeOf(p);
+        console.log(p_prototype);
+        for( prop in p_prototype ){
+            console.log(prop);
+        }
+        console.log(p_prototype === Person.prototype);
     })('getPrototyeOf');
     //=====================hasOwnProperty==================
     (function(){
         log(arguments[0]);
+        function Person( name, age ){
+            this.name = name;
+            this.age = age;
+        }
+        Person.prototype.say = function(){
+            console.log(this.name);
+        }
 
-
+        var p = new Person( 'a', 32 );
+        console.log(p.hasOwnProperty('name'));
+        console.log(p.hasOwnProperty('age'));
+        console.log(p.hasOwnProperty('say'));
     })('hasOwnProperty');
     //=====================in or for in==================
     (function(){
         log(arguments[0]);
+        function Person( name, age ){
+            this.name = name;
+            this.age = age;
+        }
+        Person.prototype.say = function(){
+            console.log(this.name);
+        }
 
-
+        var p = new Person( 'a', 32 );
+        for( prop in p ){
+            if(p.hasOwnProperty(prop)){
+                console.log('own prop:'+ prop );
+            }
+            else{
+                console.log('prototype prop:' + prop );
+            }
+        }
     })('in or for in');
+    //=====================hasPrototypeProperty==================
+    (function(){
+        log(arguments[0]);
+        var hasPrototypeProperty = function( o, name ){
+            return (name in o) && (!o.hasOwnProperty(name));
+        };
+        function Person( name, age ){
+            this.name = name;
+            this.age = age;
+        }
+        Person.prototype.say = function(){
+            console.log(this.name);
+        }
+
+        var p = new Person( 'a', 32 );
+
+        var result = hasPrototypeProperty( p, 'name' );
+        console.log(result);
+        result = hasPrototypeProperty( p, 'say' );
+        console.log(result);
+    })('hasPrototypeProperty');
 })();
